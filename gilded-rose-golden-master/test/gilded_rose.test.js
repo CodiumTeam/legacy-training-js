35d1fdb05c0350b0ded075e0c0e14aa23bf73d88
Luis Rovirosa Mairlot <luis@codium.team>
2023-09-07 22:18:13 +0200

    [gilded-rose-golden master] sample solution

--- gilded-rose-golden-master/test/gilded_rose.test.js
+++ gilded-rose-golden-master/test/gilded_rose.test.js
@@ -1,11 +1,29 @@
-import { GildedRose, Item } from '../src/gilded_rose';
+import {GildedRose, Item} from '../src/gilded_rose';
+import * as fs from "fs";
 
 describe('Gilded Rose', function () {
-  it('change me', function () {
-    const gildedRose = new GildedRose([new Item('Book', 10, 20)]);
+    it("should return correct result", () => {
+        const storeItems = [
+            new Item("+5 Dexterity Vest", 10, 20),
+            new Item("Aged Brie", 2, 0),
+            new Item("Elixir of the Mongoose", 5, 7),
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+        ];
+        const gildedRose = new GildedRose(storeItems);
 
-    const items = gildedRose.updateQuality();
+        let result = ''
+        for (let day = 0; day < 20; day++) {
+            result += `--------- day ${day}\n`;
+            result += "name, sell_in, quality\n";
+            for (const item of storeItems) {
+                result += JSON.stringify(item) + "\n";
+            }
+            gildedRose.updateQuality();
+        }
+        //fs.writeFileSync('golden_master.txt', result);
 
-    expect(items[0].name).toBe('Book');
-  });
+        let expectedResult = fs.readFileSync('golden_master.txt','utf-8');
+        expect(result).toStrictEqual(expectedResult);
+    });
 });
