2c718964d743b8ba855eab0ee129ecb5061f9adf
Luis Rovirosa Mairlot <luis@codium.team>
2024-10-18 19:33:13 +0200

    [trivia] solution

--- trivia-golden-master/src/game_runner.js
+++ trivia-golden-master/src/game_runner.js
@@ -1,19 +1,23 @@
 import Game from './game.js';
 
-var notAWinner = false;
+export function gameRunner() {
+  var notAWinner = false;
 
-var game = new Game();
+  var game = new Game();
 
-game.add('Chet');
-game.add('Pat');
-game.add('Sue');
+  game.add('Chet');
+  game.add('Pat');
+  game.add('Sue');
 
-do {
-  game.roll(Math.floor(Math.random() * 6) + 1);
+  do {
+    game.roll(Math.floor(Math.random() * 6) + 1);
 
-  if (Math.floor(Math.random() * 10) == 7) {
-    notAWinner = game.wrongAnswer();
-  } else {
-    notAWinner = game.wasCorrectlyAnswered();
-  }
-} while (notAWinner);
+    if (Math.floor(Math.random() * 10) == 7) {
+      notAWinner = game.wrongAnswer();
+    } else {
+      notAWinner = game.wasCorrectlyAnswered();
+    }
+  } while (notAWinner);
+}
+
+// gameRunner();
--- trivia-golden-master/test/game.test.js
+++ trivia-golden-master/test/game.test.js
@@ -1,8 +1,31 @@
-import Game from '../src/game';
+import {gameRunner} from "../src/game_runner.js";
+
+const {writeFileSync, readFileSync} = require('fs');
 
 describe('Game', () => {
-  it('change_this_name', () => {
-    expect(Game).toBeDefined();
-    expect(true).toBe(true);
-  });
+    it('the output remains unchanged', () => {
+        let output = "";
+        console.log = (msg) => output = output + msg + "\n";
+        Math.random = mockedRandom();
+
+        gameRunner();
+
+        // writeFileSync('golden.txt', output);
+        let expectedOutput = readFileSync('golden.txt', 'utf-8');
+        expect(output).toBe(expectedOutput);
+    });
 });
+
+let numberOfCalls = 0;
+
+function mockedRandom() {
+    numberOfCalls++;
+    return () => [
+        49, 32, 14, 71, 68, 16, 36, 85, 52, 50, 76, 18, 95, 39, 3, 77, 2, 84, 59,
+        87, 48, 43, 69, 83, 21, 37, 73, 56, 55, 58, 94, 6, 11, 46, 92, 26, 79, 38,
+        75, 57, 72, 27, 10, 53, 35, 51, 7, 81, 20, 98, 13, 40, 15, 44, 54, 78, 88,
+        19, 8, 61, 99, 47, 29, 1, 33, 24, 93, 34, 67, 23, 63, 91, 70, 22, 45, 25,
+        82, 65, 89, 31, 66, 17, 41, 74, 28, 5, 9, 97, 90, 86, 62, 96, 60, 30, 80,
+        42, 64, 4, 100,
+    ][numberOfCalls % 100] / 100;
+}
